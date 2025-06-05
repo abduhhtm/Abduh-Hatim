296dc0fbd925541407a2e02ad1ca486853fde54d
abduhhtm <abduh2605@student.abudzar.sch.id>
2025-06-05 08:52:21 +0700

    ok

---
+++ vite-project/src/SAS.jsx
@@ -0,0 +1,8 @@
+export default function SAS () {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center my-10">Ujian SAS Abduh</h1>
+      <img src="/parid.jpg" alt="" className="w-1/2 mx-auto"/>
+    </div>
+  )
+}
